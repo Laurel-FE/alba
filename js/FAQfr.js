9954ce013a7e50475d81a887404f2849ed71b279
Laurel-FE <127060063+Laurel-FE@users.noreply.github.com>
2023-03-06 01:40:44 +0200

    Initial commit

---
+++ js/FAQfr.js
@@ -0,0 +1,27 @@
+function my1(){
+document.getElementById("ans1").innerHTML="Alba utilise une approche holistique qui aborde la personne dans son ensemble plutôt que seulement ses symptômes ou problèmes. Des équipes de psychiatres et de psychologues hautement qualifiés peuvent vous aider à recadrer vos problèmes actuels en combinant plusieurs techniques de traitement et en utilisant une gamme d’outils. Par conséquent, vous serez en mesure de surmonter les obstacles et de faire face aux pressions dans une variété de sphères de votre vie. Compte tenu de la situation, Alba se consacre à atténuer les défis du counseling en personne. Nous utilisons une technologie de pointe pour vous donner facilement accès à d’excellents soins de santé mentale dans le confort de votre domicile.";
+}
+
+function my2(){
+document.getElementById("ans2").innerHTML="Des expériences de recherche récentes ont montré que le counseling en ligne peut être tout aussi bénéfique que la thérapie en personne. De plus, le counseling en ligne pourrait offrir des soins de santé mentale plus rapides et plus confortables. Des millions de clients aux États-Unis, en Europe et en Asie utilisent de nombreuses plateformes Internet. Cependant, certaines situations exigent une évaluation en personne. Par exemple, pensées suicidaires intenses, idées de nuire aux autres, symptômes de sevrage inconfortables ou toute autre situation d’urgence. À ce moment-là, nous vous suggérons de demander à vos proches de vous accompagner à l’établissement de santé mentale le plus proche.";
+}
+
+function my3(){
+document.getElementById("ans3").innerHTML="De nombreuses publications scientifiques affirment que ces 50 minutes sont la durée la plus éprouvée des séances de psychothérapie pour produire un effet significatif.";
+}
+
+function my4(){
+document.getElementById("ans4").innerHTML="Vous commencerez d’abord votre séance de thérapie en ligne avec votre thérapeute par vidéoconférence en utilisant l’application pour téléphone intelligent Alba. Après une session, vous pouvez utiliser l’application mobile Alba pour envoyer des messages directs à vos thérapeutes pendant une période de sept jours.";
+}
+
+function my5(){
+document.getElementById("ans5").innerHTML="Selon leurs organismes de réglementation et les règlements qui régissent ces professionnels, tous les thérapeutes doivent respecter la confidentialité et la vie privée, peu importe l’environnement. Toute restriction à la confidentialité sera discutée lors de votre première séance et vous sera remise par écrit dans le cadre de votre formulaire de consentement à la thérapie. En outre, les thérapeutes doivent avoir accès à une zone privée sécurisée où ils peuvent effectuer une thérapie.";
+}
+
+function my6(){
+document.getElementById("ans6").innerHTML="Jusqu’à 24 heures avant votre rendez-vous, vous pouvez reporter ou annuler votre session.";
+}
+
+function my7(){
+document.getElementById("ans7").innerHTML="Vous devrez entrer les détails de votre carte de paiement tout au long du processus d’inscription. Vous pouvez uniquement utiliser les cartes de crédit et de débit pour effectuer des paiements à ce moment. Les transferts bancaires, Paypal et les chèques ne sont pas acceptés.";
+}
